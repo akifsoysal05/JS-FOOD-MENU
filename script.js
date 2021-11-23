5719e92a80a22e97b08db9aeda9534438727e9a5
akifsoysal05 <92169213+akifsoysal05@users.noreply.github.com>
2021-11-23 13:23:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const menuBtns = document.querySelector('.btn-container');
+const foodItems = document.querySelector('menu-items');
+
+let activeBtn = "featured";
+
+showFoodMenu(activeBtn)
+
+menuBtns.forEach(((btn) => {
+    btn.addEventListener('click', () => {
+        resetActiveBtn()
+        showFoodMenu(btn.id)
+        btn.classList.add('active-btn')
+    } );
+}),
+
+function resetActiveBtn () {
+    menuBtns.forEach((btn) => {
+        btn.classList.remove('active-btn')
+    });
+})
+
+function showFoodMenu(newMenuBtn) {
+    activeBtn = newMenuBtn
+    foodItems.forEach((item) => {
+        if(item.classList.contains(activeBtn)) {
+            item.style.display = "grid"
+        }
+        else {
+            item.style.display = "none"
+        }
+    });
+} 
